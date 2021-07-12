5cdfc968b7041bfca9d5b38ebdfdbc9f3491dcaf
t4kuya <84721993+t4kuya@users.noreply.github.com>
2021-07-12 14:25:33 +0900

    Update credate.rb

--- credate.rb
+++ credate.rb
@@ -1,1 +1,12 @@
+VERSION="0.0.0"
 
+class credate < Formula
+  desc "Improved version of wc"
+  homepage "https://github.com/t4kuya/credate"
+  url "https://github.com/t4kuya/credate/releases/download/v#{VERSION}/credate-#{VERSION}_darwin_amd64.tar.gz"
+  version VERSION
+  
+  def install
+    bin.install "credate"
+  end
+end
