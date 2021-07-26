0a70519bb561c58e22ee6b93ec937c11ed97ef8f
t4kuya <aaa_1d_1115@icloud.com>
2021-07-26 20:08:16 +0900

    update

--- credate.rb
+++ credate.rb
@@ -6,7 +6,7 @@ class Credate < Formula
   url "https://github.com/t4kuya/credate/releases/download/#{VERSION}/credate-#{VERSION}_darwin_amd64.tar.gz"
   version VERSION
   
-  sha256 "b6276615b5268e2a0323e3312e3fc936b24c78dd99222603e04acb8ec74cb9c6"
+  sha256 "9222d4ca6577f7d006ab182cc08b627cde4806b4db655e89d4c066f3c9a7eb91"
 
   def install
     bin.install "credate"
